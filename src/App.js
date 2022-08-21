c49c24ba3ea985afa963b152c8512d5a6944eafb
Davud Gayibov <davud8888@yahoo.com>
2022-08-21 19:06:24 +0200

    logo added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import icon from './Square_Black.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>
+        coming soon<span className="dot"></span>
+      </h1>
+      <br></br>
+      <div>
+        <img id="logo" data-testid="logo" src={icon} alt="icon" />
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders coming soon page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const comingSoonText = screen.getByText(/coming soon/i);
+  const logo = screen.getByTestId('logo');
+  expect(comingSoonText).toBeInTheDocument();
+  expect(logo).toBeInTheDocument();
 });
